7da0ae210f9aa57feb86d2583f4b9e0db32df01f
Pascal <pascal@wengerter.info>
2020-03-11 12:33:45 +0100

    Further refactoring

--- tabata_app.rb
+++ tabata_app.rb
@@ -1,4 +1,4 @@
-# my_app.rb
+# tabata_app.rb
 
 require 'sinatra/base'
 require 'json'
@@ -15,34 +15,28 @@ class TabataApp < Sinatra::Base
   # TODO: Whitelist params: Expects 2 audio files and two starting-points
   post '/api/v1' do
 
-    active_track = params['active_track']
-    rest_track = params['rest_track']
-    tmp_final_track_name = params['active_track'][:tempfile].path + params['rest_track'][:tempfile].path
-    tmp_final_track_name = tmp_final_track_name.gsub('.mp3', '')
-    tmp_final_track_name = tmp_final_track_name.gsub('/tmp/', '')
+    tmp_final_track_name = (params['active_track'][:tempfile].path + params['rest_track'][:tempfile].path).gsub('.mp3', '').gsub('/tmp/', '')
     active_tempfile = params['active_track'][:tempfile]
     rest_tempfile = params['rest_track'][:tempfile]
 
     active_interval = params['active_interval'].split(',')
     rest_interval = params['rest_interval'].split(',')
+    # TODO: Remove endpoints because they can be manually calculated
     active_startingpoint = active_interval[0]
     active_endpoint = active_interval[1]
     rest_startingpoint = rest_interval[0]
     rest_endpoint = rest_interval[1]
 
-    # TODO: Remove endpoints because they can be manually calculated
     active_part = create_active_track(active_tempfile, active_startingpoint, active_endpoint)
     passive_part = create_rest_track(rest_tempfile, rest_startingpoint, rest_endpoint)
-
     one_interval = concat_tracks('active_output', 'rest_output')
-
     final_track = create_final_track('one_output', tmp_final_track_name)
+
+    delete_tracks [active_tempfile.path, rest_tempfile.path, 'active_output', 'rest_output', 'one_output']
 
     if true
       send_file "/tmp/#{tmp_final_track_name}.mp3", :filename => 'tabata_track.mp3', :type => 'audio/mpeg', disposition: 'attachment'
-      # line below possibly never reached
-      # come up with possiblity to delete tmp_final_track_name as after-action or cron
-      delete_track tmp_final_track_name
+      # TODO: Delete tmp final track
     else
       # TODO: Come up with error handling
       status 500
@@ -67,17 +61,21 @@ class TabataApp < Sinatra::Base
 
   def concat_tracks first, second
     system "ffmpeg -i \"concat:/tmp/#{first}.mp3|/tmp/#{second}.mp3\" -c copy /tmp/one_output.mp3"
-    delete_track 'active_output'
-    delete_track 'rest_output'
   end
 
   def create_final_track track, track_name
     system "ffmpeg -y -i \"concat:/tmp/#{track}.mp3|/tmp/#{track}.mp3|/tmp/#{track}.mp3|/tmp/#{track}.mp3|/tmp/#{track}.mp3|/tmp/#{track}.mp3|/tmp/#{track}.mp3|/tmp/#{track}.mp3\" -c copy /tmp/#{track_name}.mp3"
-    delete_track 'one_output'
   end
 
   def delete_track track_name
     File.delete("/tmp/#{track_name}.mp3") if File.exist?("/tmp/#{track_name}.mp3")
+    File.delete("#{track_name}") if File.exist?("#{track_name}")
+  end
+
+  def delete_tracks tracks
+    tracks.each do |track|
+      delete_track track
+    end
   end
 
   # start the server if ruby file executed directly
